4122673463c59759b1e519773f02f88bba8c0468
cat538 <1350359575@qq.com>
2022-10-30 23:38:59 +0800

    initialization

---
+++ lua/user/options.lua
@@ -0,0 +1,46 @@
+local options = {
+  backup = false,                          -- creates a backup file
+  swapfile = false,                        -- creates a swapfile
+  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
+  cmdheight = 1,                           -- 命令行高度 
+  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+  conceallevel = 0,                        -- so that `` is visible in markdown files
+  fileencoding = "utf-8",                  -- 文件编码格式设置为utf-8
+  hlsearch = true,                         -- 高亮所有maches 
+  ignorecase = true,                       -- 搜索时大小写不敏感
+  smartcase = true,                        -- 存在大写则大小写敏感,否则大小写不敏感
+  mouse = "a",                             -- 允许使用鼠标
+  pumheight = 10,                          -- pop up menu height
+  showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
+  showtabline = 2,                         -- always show tabs
+  smartindent = true,                      -- make indenting smarter again
+  splitbelow = true,                       -- force all horizontal splits to go below current window
+  splitright = true,                       -- force all vertical splits to go to the right of current window
+  termguicolors = true,                    -- set term gui colors (most terminals support this)
+  timeoutlen = 1000,                        -- time to wait for a mapped sequence to complete (in milliseconds)
+  undofile = true,                         -- enable persistent undo
+  updatetime = 300,                        -- faster completion (4000ms default)
+  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+  expandtab = true,                        -- convert tabs to spaces
+  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
+  tabstop = 2,                             -- 设置tab为两个空格 
+  cursorline = true,                       -- 高亮当前行 
+  number = true,                           -- 显示行号 
+  relativenumber = true,                   -- 显示相对行号
+  numberwidth = 4,                         -- set number column width to 2 {default 4}
+  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
+  wrap = false,                            -- display lines as one long line
+  scrolloff = 8,                           -- is one of my fav
+  sidescrolloff = 8,
+  guifont = "monospace:h17",               -- the font used in graphical neovim applications
+}
+
+vim.opt.shortmess:append "c"
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
+vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd [[set iskeyword+=-]]
+vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
