f5990fe79d192e890bd3e5feb11f729322dcd97d
cat538 <1350359575@qq.com>
2022-11-03 15:21:09 +0800

    feat: add lualine

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -50,6 +50,7 @@ return packer.startup(function(use)
   use "nvim-lua/plenary.nvim"     -- Useful lua functions used by lots of plugins
   use "akinsho/bufferline.nvim"   -- buffer line (with tabpage integration)
   use "akinsho/toggleterm.nvim"   -- use outer terminal in nvim
+  use "nvim-lualine/lualine.nvim" -- bottom bar
 
   -- Colorschemes
   use "folke/tokyonight.nvim"
